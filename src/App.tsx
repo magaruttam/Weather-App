8494ff83a78ed79c37c712e310a4019505be3e53
Uttam Pulami <pulamimagaruttam@gmail.com>
2026-06-26 09:48:32 +0545

    api key change

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import {useQuery} from '@tanstack/react-query'
+import { getWeatherData } from './api';
+
 function App() {
+
+  const {data} = useQuery({
+    queryKey: ['weatherData'],
+    queryFn: ()=> getWeatherData(37.7749, -122.4194)
+  })
+    console.log(data)
   return (
     <>
-    
+    {JSON.stringify(data)}
     </>
   )
 }
-
 export default App
--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
 const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
 
 export async function getWeatherData(lat: number, lon: number) {
-    const response = await fetch(`https://api.openweathermap.org/data/4.0/onecall/current?lat=${lat}&lon=${lon}&appid=${apiKey}`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`);
     return response.json();
 }
