3571ff85c84bac7ec74a8d9b20406d744286fe90
Uttam Pulami <pulamimagaruttam@gmail.com>
2026-06-28 09:37:53 +0545

    weather schema

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import {useQuery} from '@tanstack/react-query'
 import { getWeatherData } from './api';
+import Card from './components/cards/card';
 
 function App() {
 
   const {data} = useQuery({
     queryKey: ['weatherData'],
-    queryFn: ()=> getWeatherData(37.7749, -122.4194)
+    queryFn: ()=> getWeatherData(28.6026, 81.6330)
   })
     console.log(data)
   return (
-    <>
-    {JSON.stringify(data)}
-    </>
+    <div className="flex flex-col gap-8">
+      <Card title="Coordinates">{JSON.stringify(data?.coord)}</Card>
+      <Card title="Weather">{JSON.stringify(data?.weather)}</Card>
+    </div>
   )
 }
 export default App
--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,9 @@
+import { WeatherSchema } from "./schemas/weatherSchema";
+
 const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
 
 export async function getWeatherData(lat: number, lon: number) {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`);
-    return response.json();
+    const data = await response.json();
+    return WeatherSchema.parse(data);
 }
